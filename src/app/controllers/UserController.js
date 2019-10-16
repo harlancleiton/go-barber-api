56c23bf7657615a9558b1b45efec69d767dde6e9
Harlan Cleiton <harlancleiton@gmail.com>
2019-10-16 14:56:34 -0300

    Auth middleware

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,6 +10,10 @@ class UserController {
 
     return res.json({ id, name, email, provider });
   }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
