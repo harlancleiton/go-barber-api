c5c6dd1694410f869fb64273c7bd01d6c7bc0201
Harlan Cleiton <harlancleiton@gmail.com>
2019-10-18 09:46:13 -0300

    POST appointments

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,5 +23,6 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
